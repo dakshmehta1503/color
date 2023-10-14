fe2516cd617ce43acd5d1134a3f0717ed960a058
dakshmehta1503 <99042632+dakshmehta1503@users.noreply.github.com>
2023-10-14 23:52:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let globalc = "black"
+  let[clr, setClr] = useState(globalc);
+
+  function chng(){
+    let colr = "red"
+    setClr(colr)
+  }
+  function chng1(){
+    let colr1 = "blue"
+    setClr(colr1)
+  }
+  function chng2(){
+    let colr2 = "green"
+    setClr(colr2)
+  }
+  function chng3(){
+    let colr3 = "Gray"
+    setClr(colr3)
+  }
+  function chng4(){
+    let colr4 = "White"
+    setClr(colr4)
+  }
+  function chng5(){
+    let colr5 = "Lavender"
+    setClr(colr5)
+  }
+  function chng6(){
+    let colr6 = "#fa90a2"
+    setClr(colr6)
+  }
+  function chng7(){
+    let colr7 = "Purple"
+    setClr(colr7)
+  }
+  return(
+      <div className="toolbox" style={{backgroundColor:clr}}>
+        <div className="box">
+          <button onClick={chng} className="btnbtn1">Red</button>
+          <button onClick={chng1} className="btnbtn2">Blue</button>
+          <button onClick={chng2} className="btnbtn3">Green</button>
+          <button onClick={chng3} className="btnbtn4">Gray</button>
+          <button onClick={chng4} className="btnbtn5">White</button>
+          <button onClick={chng5} className="btnbtn6">Pink</button>
+          <button onClick={chng6} className="btnbtn7">Purple</button>
+          <button onClick={chng7} className="btnbtn8">Lavender</button>
+        </div>
+      </div>
+  )
 }
 
-export default App;
+export default App
